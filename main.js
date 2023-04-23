acc8e926dadbdb31ae60b125e6c5e4c75a85512a
Dalton Gray <daltonagray@gmail.com>
2023-04-23 19:19:43 +0100

    Basic functionality working

---
+++ main.js
@@ -0,0 +1,42 @@
+symbolBank = ["a", "b", "c", "d", "e"];
+target = symbolBank[Math.floor(Math.random()*symbolBank.length)];
+
+
+function incorrect() {
+    currentVal = parseInt(document.getElementById("incorrect").textContent);
+    document.getElementById("incorrect").textContent = currentVal +1;
+}
+
+function correct() {
+    currentVal = parseInt(document.getElementById("correct").textContent);
+    document.getElementById("correct").textContent = currentVal +1;
+}
+
+
+function checkAnswer(target, answer) {
+    return target == answer;
+}
+function updateTargetHtml(target) {
+    document.getElementById("target-symbol").textContent = target;
+
+}
+function clearInput() {
+    document.getElementById("input").value = '';
+}
+
+
+
+function nextSymbol() {
+    // window.alert("test");
+    answer = document.getElementById("input").value
+    console.log(answer);
+    if (checkAnswer(target, answer)) {
+        correct();
+    } else {
+        incorrect();
+    }
+    target = symbolBank[Math.floor(Math.random()*symbolBank.length)];
+    updateTargetHtml(target)
+    clearInput()
+    console.log(target);
+}
